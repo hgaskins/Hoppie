2ea58906217dc341938afff80ade6e769d8b5986
rgylling <mikegylling@yahoo.com>
2016-05-02 15:30:25 -0700

    added zero out statement

--- js/bac.js
+++ js/bac.js
@@ -1,14 +1,18 @@
 // (function(module) {
 
-
-  $('#bacSubmit').on("click" , function () {
+  $('#bacSubmit').on('click' , function () {
     var drink = $('#drinksOutput').val();
     var percent = $('#percentageOutput').val();
     var weights = $('#weightOutput').val();
     var hour = $('#hoursOutput').val();
     var bacTotal = ((drink * 12 * percent * 0.075 / weights) - (hour * 0.015));
     var bac = Math.round(bacTotal * 100) / 100;
-    $('#bacVal').val(bac);
+    if (bac < 0){
+      $('#bacVal').val(0);
+    }
+    else {
+      $('#bacVal').val(bac);
+    }
   });
 
 

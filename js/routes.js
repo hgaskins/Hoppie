8d0cb0177dd4e69228521966c3987130297b87e5
Hannah Gaskins <gaskinshannah@gmail.com>
2016-05-02 09:49:28 -0700

    added routes.js page and changed the file names brewery page to breweries

---
+++ js/routes.js
@@ -0,0 +1,15 @@
+(function(module) {
+  var routes = {};
+  routes.setMapping = function() {
+    page.base('/');
+
+    page('', homeController.index);
+    page('breweries', breweriesController.index);
+    page('about', aboutController.index);
+    page('bac', bacController.index);
+
+    page();
+  };
+  routes.setMapping();
+  module.routes = routes;
+})(window);

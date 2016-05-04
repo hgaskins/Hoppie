ad0364b878465d8903ebbeb15b5e1a7d3df950a9
Johnny Luangphasy <johnny@jluangphasy.com>
2016-05-04 09:14:25 -0700

    Made off canvas 100% height of viewport, setup REST routes in server.js

--- js/homeController.js
+++ js/homeController.js
@@ -4,13 +4,15 @@
   homeController.index = function() {
     $('.homePage').show().siblings().hide();
 
+    $('#offCavnas').foundation('close');
+
     $('.js-button-search').off('click').on('click', function () {
       $button = $(this).parent('.input-group-button').siblings('.input-group-field');
       var searchValue = $button.val().trim();
 
       if (searchValue.length) {
         page.redirect('/breweries/' + searchValue.toLowerCase().replace(' ', '-'));
-        $button.val('');
+        // $button.val('');
       }
     });
   };

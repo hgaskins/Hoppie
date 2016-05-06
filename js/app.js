4074b49c1d39c171fa056569ff3e142ce875b504
Johnny Luangphasy <johnny@jluangphasy.com>
2016-05-06 11:51:08 -0700

    Added section comments to JS files

--- js/app.js
+++ js/app.js
@@ -1,8 +1,16 @@
 $(document).ready(function() {
+
+  // Init Foundation
   $(this).foundation();
 
+  // Init search
   searchView.handleSearchInput();
   searchView.handleSearchButton();
+
+  // Init BAC
   bacView.calcBac();
+
+  // Init routes
   routes.setMapping();
+
 });

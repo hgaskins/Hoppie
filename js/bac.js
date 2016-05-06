61e1b35463285a1e522567f19acf88ae704eea18
rgylling <mikegylling@yahoo.com>
2016-05-06 10:28:03 -0700

    added modal to bac page

--- js/bac.js
+++ js/bac.js
@@ -6,11 +6,10 @@
      $('.bacPage').show().siblings().hide();
    };
 
-
    var button = $('#bacSubmit');
    var audio = $('audio');
    var bacOutput = $('#bacVal');
-
+   var bacOutput1 = $('#bacVal1');
 
 
    function unpress(){
@@ -26,19 +25,16 @@
      var bacTotal = ((drink * 12 * percent * 0.075 / weights) - (hour * 0.015));
      var bac = Math.round(bacTotal * 100) / 100;
      if (bac && bac >= 0 && bac <= 0.7){
-       bacOutput.text('BAC = ' + bac + '%').css('color','black');
-       console.log(bac + 'your bac is below .7');
+       bacOutput1.text('BAC = ' + bac + '%').css('color','black');
      }else if(bac && bac >= 0 && bac > 0.7){
-       bacOutput.text('WARNING YOUR BAC IS ' + bac + '% ').css('color','red');
-       console.log(bac + 'WARNING YOURE DRUNK');
+       bacOutput.text('WARNING YOUR BAC IS ' + bac + '% click on Hoppie for a ride!').css('color','red');
+       $('#exampleModal1').foundation('open');
      } else {
-       bacOutput.text('BAC = 0%').css('color','black');
-       console.log(bac + 'your bac is 0');
+       bacOutput1.text('BAC = 0%').css('color','black');
      }
      audio.get(0).play();
      button.addClass('pressed');
      setTimeout(unpress,3000);
-
    });
 
 
